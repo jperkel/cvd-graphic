dcf5ce3a396e50a44fc60847e0940250ff19aa8e
Jeffrey Perkel <j_perkel@yahoo.com>
2021-10-07 08:35:00 -0600

    Save output files

--- cvd_color_example.R
+++ cvd_color_example.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(colorBlindness)
+library(here)
 
 # from https://github.com/clauswilke/colorblindr/blob/master/R/palettes.R
 palette_OkabeIto <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7", "#999999")
@@ -10,10 +11,13 @@ p <- ggplot(diamonds, aes(x = price, fill = cut)) +
   geom_histogram() +
   labs(x = "Price (USD)", y = "Count")
 # Check output in a CVD simulator
-cvdPlot(p)
+std_palette <- cvdPlot(p)
+ggsave(here("std_palette.jpg"), width = 8, height = 6, units = "in")
 
 # Okabe-Ito palette... check output in a CVD simulator
-cvdPlot(p + scale_fill_manual(values = palette_OkabeIto))
+okabe_ito <- cvdPlot(p + scale_fill_manual(values = palette_OkabeIto))
+ggsave(here("okabe_ito.jpg"), width = 8, height = 6, units = "in")
 
 # rainbow palette... check output in a CVD simulator
-cvdPlot(p + scale_fill_manual(values = rainbow(5)))
+rainbow <- cvdPlot(p + scale_fill_manual(values = rainbow(5)))
+ggsave(here("rainbow.jpg"), width = 8, height = 6, units = "in")
